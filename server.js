814411bca107a0ebb9a9ef9bac92a7fdd119a747
Ferruccio Bisceglia <ferruccio.bisceglia@icubed.it>
2023-06-24 20:16:42 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+
+// Define your array of values
+const values = ['italia', 'spaghetti', 'mafia'];
+
+// Define a route that returns the array of values
+app.get('/api/values', (req, res) => {
+  res.json(values);
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
